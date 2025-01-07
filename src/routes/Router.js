1f88a28b489e5f80ebd532b5386e0077ce0b7b98
MaN0r@j$ <manorajsivanmalai>
2025-01-07 20:40:03 +0530

    error cleared

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,20 +1,19 @@
-import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 
-/****Layouts*****/
-const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
+/**** Layouts ****/
+import FullLayout from "../layouts/FullLayout.js";
 
 /***** Pages ****/
+import Starter from "../views/Starter.js";
+import CollectionAmount from "../views/CollectionsAmount.js";
+import AddMembers from "../views/AddMembers.js";
+import MemberDeails from "../components/MemberDeails";
+import ShowExpenses from "../views/ShowExpenses";
+import Expenses from "../views/Expenses.js";
+import ExpensesDetails from "../components/Expensesdetails.jsx";
+import Login from "../views/Login.js";
 
-const Starter = lazy(() => import("../views/Starter.js"));
-const CollectionAmount = lazy(() => import("../views/CollectionsAmount.js"));
-const AddMembers = lazy(() => import("../views/AddMembers.js"));
-const MemberDeails = lazy(() => import("../components/MemberDeails"));
- const ShowExpenses = lazy(() => import("../views/ShowExpenses"));
-const Expenses = lazy(() => import("../views/Expenses.js"));
-const ExpensesDetails = lazy(()=> import("../components/Expensesdetails.jsx"));
-const Login =lazy(()=> import("../views/Login.js"));
-/*****Routes******/
+/***** Routes *****/
 
 const ThemeRoutes = [
   {
@@ -22,14 +21,14 @@ const ThemeRoutes = [
     element: <FullLayout />,
     children: [
       { path: "/", element: <Navigate to="/starter" /> },
-      { path: "/starter", exact: true, element: <Starter /> },
-      { path: "/collectionAmount", exact: true, element: <CollectionAmount /> },
-      { path: "/addmembers", exact: true, element: <AddMembers /> },
-      { path: "/memberdeails", exact: true, element: <MemberDeails /> },
-      { path:"/expensesdetails",exact:true,element:<ExpensesDetails />},
-      { path: "/showexpenses", exact: true, element: <ShowExpenses /> },
-      { path: "/expenses", exact: true, element: <Expenses /> },
-      { path: "/login", exact: true, element: <Login /> },
+      { path: "/starter", element: <Starter /> },
+      { path: "/collectionAmount", element: <CollectionAmount /> },
+      { path: "/addmembers", element: <AddMembers /> },
+      { path: "/memberdeails", element: <MemberDeails /> },
+      { path: "/expensesdetails", element: <ExpensesDetails /> },
+      { path: "/showexpenses", element: <ShowExpenses /> },
+      { path: "/expenses", element: <Expenses /> },
+      { path: "/login", element: <Login /> },
     ],
   },
 ];
