e0fe4754a0ead44ab01654f4e3e26c2e12141638
Mano <manorajsivanmalai>
2025-01-09 17:49:32 +0530

    testing2

--- netlify/functions/backend.js
+++ netlify/functions/backend.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const router = express.Router();
 const serverless = require('serverless-http');
-// const { createMember, getMembers,updateMember,deleteMember } = require('../controller/MemberController');
+ const { createMember, getMembers,updateMember,deleteMember } = require('../controller/MemberController');
 // const {createExpenses,getExpenses,updateExpenses, deleteExpenses} = require('../controller/ExpensesController');
  const {userRouter} =require("../controller/UserController");
 require('dotenv').config();
@@ -15,10 +15,10 @@ router.get("/",(req,res)=>{res.json("server is running")})
 
 
 //member Routers
-// router.post("/createmember",createMember);
-// router.get("/getmembers",getMembers);
-// router.put("/member/:id",updateMember);
-// router.delete("/member/:id",deleteMember);
+router.post("/createmember",createMember);
+router.get("/getmembers",getMembers);
+router.put("/member/:id",updateMember);
+router.delete("/member/:id",deleteMember);
 
 // //expenses router
 // router.post("/addexpense",createExpenses);
@@ -28,7 +28,7 @@ router.get("/",(req,res)=>{res.json("server is running")})
 
 
 app.use('/api/', router);
- app.use("/api/",userRouter);
+app.use("/api/",userRouter);
 
 
 
