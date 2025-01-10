9f49b205b5d5d15fd322adf6bc94dbeb4504f1cf
Mano <manorajsivanmalai>
2025-01-10 10:29:41 +0530

    db connctin1

--- netlify/functions/backend.js
+++ netlify/functions/backend.js
@@ -12,25 +12,27 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-router.get("/",async (req,res)=>{
-   
+const sequelize = new Sequelize("postgres://default:oQm2pSXI5ulY@ep-patient-rain-a4vsiaah-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
+    dialect: 'postgres',
+    logging: false,
+  });
+  
+  app.get('/', async (req, res) => {
     try {
-// Option 1: Passing a connection URI
-const sequelize = new Sequelize("postgres://default:oQm2pSXI5ulY@ep-patient-rain-a4vsiaah-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require") 
-
-
-    await sequelize.authenticate();
-    res.json('Connection has been established successfully.');
-  } catch (error) {
-    res.json('Unable to connect to the database:', error);
-  }
+      await sequelize.authenticate();
+      res.status(200).send('Database connected successfully');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      res.status(500).send('Unable to connect to the database');
+    }
+  });
 // Option 3: Passing parameters separately (other dialects)
 // const sequelize = new Sequelize('database', 'username', 'password', {
 //   host: 'localhost',
 //   dialect: 'postgres'
 // });
 
-})
+
 
 
 //member Routers
